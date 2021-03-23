fa7654e744fc5d5e8cddf6a62544b829104afa18
Sergey Smirnov <serasmi@yandex.ru>
2021-03-23 23:20:16 +0300

    (project) Add project files.

---
+++ swift-firebase-chat/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  swift-firebase-chat
+//
+//  Created by Сергей Смирнов on 23.03.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
